0fd40895686d992b4fda9f708903f13fa2300e72
Barrett Smith <barrett@datasmith.net>
2023-12-21 08:07:40 -0500

    Add functionality for handling a cover letter template and to be more DRY.

--- resume_helper.py
+++ resume_helper.py
@@ -8,6 +8,9 @@ Code _borrowed_ heavily from https://developers.google.com/docs/api/quickstart/p
 _Caveat Emptor_: I haven't tested by special characters in the modifier may cause issues.
 
 Set up: Follow the instructions for setting up an application and downloading a credentials.json in the google link above.
+
+@todo Add handling for a cover letter also
+@todo Define a text delimiter and replace text in the resume and cover letter with a text string
 """
 
 import argparse
@@ -23,10 +26,11 @@ from googleapiclient.errors import HttpError
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 
 # The ID of base resume document.
-DOCUMENT_ID = "1UqHEDcDvv-cL9t40dtMT0OYI4Z8hqpuPDPoV0llIDII"
+RESUME_DOCUMENT_ID = "1aZAY2BK0lA7cDR7V6u1UG7__xsC1d_5sigXj9PO1-yc"
+COVERLETTER_DOCUMENT_ID = "1fo-3MXG_Nitq1T5WQV-FHN1ePp2rVGBrZSYNWI_L6x4"
 
 
-def main(modifier: str):
+def main(company_name: str, role_name: str):
   """Shows basic usage of the Docs API.
   Prints the title of a sample document.
   """
@@ -52,14 +56,28 @@ def main(modifier: str):
   try:
     service = build("drive", "v3", credentials=creds)
 
-    new_file_name = f"Resume - {modifier}"
-    document = service.files().copy(fileId=DOCUMENT_ID, body={'name': new_file_name} ).execute()
+    # todo: Refactor this to be DRY
+    new_resume_name = _build_document_name('resume', company_name, role_name)
+    resume = _copy_document(service, RESUME_DOCUMENT_ID, new_resume_name)
 
-    file_url = f"https://docs.google.com/document/d/{document.get('id')}/edit"
+    resume_file_url = f"https://docs.google.com/document/d/{resume.get('id')}/edit"
 
-    print(f"The new document is: {file_url}")
+    new_coverletter_name = _build_document_name('coverletter', company_name, role_name)
+    coverletter = _copy_document(service, COVERLETTER_DOCUMENT_ID, new_coverletter_name)
+
+    coverletter_file_url = f"https://docs.google.com/document/d/{coverletter.get('id')}/edit"
+
+    print(f"The new resume is: {resume_file_url}")
+    print(f"The new cover letter is: {coverletter_file_url}")
+    # end refactor todo
+
   except HttpError as err:
     print(err)
+
+def _copy_document(service, document_id: str, new_name: str):
+  new_document = service.files().copy(fileId=document_id, body={'name': new_name} ).execute()
+
+  return new_document
 
 def _parse_args() -> argparse.Namespace:
     """
@@ -69,14 +87,31 @@ def _parse_args() -> argparse.Namespace:
     """
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('company_role',
-                        help="The company and role information to be added to the title of the new file."
+    parser.add_argument('company_name',
+                        help="The company name to be added to the title of the new file."
+                        )
+    
+    parser.add_argument('role_name',
+                        help="The role name to be added to the title of the new file."
                         )
 
     return parser.parse_args()
 
+def _build_document_name(document_type: str, company_name: str, role_name: str) -> str:
+    match document_type:
+      case 'resume':
+        type_name = 'Resume'
+      case 'coverletter':
+        type_name = 'Cover Letter'
+      case _:
+        raise ValueError(f'Document Type must be one of resume or coverletter. Got {document_type}')
+    
+    document_name = f'{type_name} - {company_name} - {role_name}'
+
+    return document_name
 
 if __name__ == "__main__":
-  file_modifier = vars(_parse_args()).pop('company_role')
+  company_name = vars(_parse_args()).pop('company_name')
+  role_name = vars(_parse_args()).pop('role_name')
 
-  main(file_modifier)
+  main(company_name, role_name)
