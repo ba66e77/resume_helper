4b516d8636e29e6ec235920bc607f59047b92b65
Barrett Smith <barrett@datasmith.net>
2023-12-22 17:50:50 -0500

    Refactoring to add type hinting and for reuse.

--- resume_helper.py
+++ resume_helper.py
@@ -10,7 +10,7 @@ import os.path
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
+from googleapiclient.discovery import build, Resource
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
@@ -23,31 +23,40 @@ SCOPES = [
 RESUME_DOCUMENT_ID = "1aZAY2BK0lA7cDR7V6u1UG7__xsC1d_5sigXj9PO1-yc"
 COVERLETTER_DOCUMENT_ID = "1fo-3MXG_Nitq1T5WQV-FHN1ePp2rVGBrZSYNWI_L6x4"
 
-
-def main(company_name: str, role_name: str):
-  """Shows basic usage of the Docs API.
-  Prints the title of a sample document.
-
-  @todo refactor creds functionality out to separate function
-  """
+def _get_credentials(token_file_path: str = "token.json", credentials_file_path: str = "credentials.json") -> Credentials:
   creds = None
+
   # The file token.json stores the user's access and refresh tokens, and is
   # created automatically when the authorization flow completes for the first
   # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  if os.path.exists(token_file_path):
+    creds = Credentials.from_authorized_user_file(token_file_path, SCOPES)
+
   # If there are no (valid) credentials available, let the user log in.
   if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
       creds.refresh(Request())
     else:
       flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
+          credentials_file_path, SCOPES
       )
       creds = flow.run_local_server(port=0)
+
     # Save the credentials for the next run
-    with open("token.json", "w") as token:
+    with open(token_file_path, "w") as token:
       token.write(creds.to_json())
+
+  return creds
+
+def _build_document_url(document_id: str) -> str:
+  return f"https://docs.google.com/document/d/{document_id}/edit"
+
+def main(company_name: str, role_name: str) -> None:
+  """Shows basic usage of the Docs API.
+  Prints the title of a sample document.
+
+  """
+  creds = _get_credentials()
 
   try:
     drive_service = build("drive", "v3", credentials=creds)
@@ -59,14 +68,14 @@ def main(company_name: str, role_name: str):
     resume = _copy_document(drive_service, RESUME_DOCUMENT_ID, new_resume_name)
 
     resume_id = resume.get('id')
-    resume_file_url = f"https://docs.google.com/document/d/{resume_id}/edit"
+    resume_file_url = _build_document_url(resume_id)
     # end encapsulate todo
 
     new_coverletter_name = _build_document_name('coverletter', company_name, role_name)
     coverletter = _copy_document(drive_service, COVERLETTER_DOCUMENT_ID, new_coverletter_name)
 
     coverletter_id = coverletter.get('id')
-    coverletter_file_url = f"https://docs.google.com/document/d/{coverletter_id}/edit"
+    coverletter_file_url = _build_document_url(coverletter_id)
 
     print(f"The new resume is: {resume_file_url}")
     print(f"The new cover letter is: {coverletter_file_url}")
@@ -80,11 +89,13 @@ def main(company_name: str, role_name: str):
   except HttpError as err:
     print(err)
 
-def _replace_text(document_service, document_id: str, company_name: str, role_name: str, date_string: datetime):
+def _replace_text(document_service: Resource, document_id: str, company_name: str, role_name: str, date_string: str) -> dict:
   """
   cover letter fields: {{date}}, {{title}}, {{company_name}}
 
   resume fields: {{title}}
+
+  @todo: pass in a dict of replacement keys and replacement values, to be more reusable.
   """
 
   requests = [
@@ -124,7 +135,7 @@ def _replace_text(document_service, document_id: str, company_name: str, role_na
   
   return result
 
-def _copy_document(service, document_id: str, new_name: str):
+def _copy_document(service: Resource, document_id: str, new_name: str) -> dict:
   new_document = service.files().copy(fileId=document_id, body={'name': new_name} ).execute()
 
   return new_document
